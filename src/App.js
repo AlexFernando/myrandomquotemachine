6ccdb5108244ba2133a56a1d331a868c855588b9
alexfernando <alex.fernando.801@gmail.com>
2019-08-05 18:00:07 +0000

    responsivness and other styling fixes

--- src/App.js
+++ src/App.js
@@ -13,14 +13,13 @@ class App extends Component {
     this.state = {
       quotes : [],
       colors: ['#FF6633', '#FF33FF', '#00B3E6', '#E6B333', 
-      '#3366E6', '#999966', '#99FF99', '#B34D4D', '#80B300', 
-      '#809900', '#6680B3', '#66991A', '#FF99E6', '#FF1A66', 
-      '#E6331A', '#66994D', '#B366CC', '#4D8000', '#B33300', 
-      '#CC80CC', '#66664D', '#991AFF', '#E666FF', '#4DB3FF', 
-      '#1AB399', '#E666B3', '#33991A', '#CC9999', '#B3B31A', 
-      '#00E680', '#4D8066', '#809980', '#999933', '#FF3380', 
-      '#66E64D', '#4D80CC', '#9900B3', '#E64D66', '#4DB380', 
-      '#FF4D4D', '#6666FF',
+      '#3366E6', '#999966', '#B34D4D', '#80B300', '#6666FF',
+      '#809900', '#6680B3', '#66991A', '#FF1A66', '#E6331A', 
+      '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+      '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399', 
+      '#E666B3', '#33991A', '#B3B31A', '#4D8066', '#809980', 
+      '#999933', '#FF3380', '#4D80CC', '#9900B3', '#E64D66', 
+      '#4DB380', '#FF4D4D'
       ],
       quoteText: 'There is no quote yet',
       quoteAuthor: 'Anonymous',
@@ -73,6 +72,10 @@ class App extends Component {
   
         
       <div className="parent">
+
+        <div className="title">
+          <h2>The Random Quote Machine</h2>
+        </div>
         <div className="card">
           <Quote
             quoteText = {this.state.quoteText}
@@ -87,7 +90,7 @@ class App extends Component {
         </div>
 
         <div className="developer">
-          <span>By Alex Fernando</span>
+          <span>~ By Alex Fernando</span>
         </div>
       </div>
       
